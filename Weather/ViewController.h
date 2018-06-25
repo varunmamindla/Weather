becfdbb74be5dc55ace935821059de04dcce9e09
Varun Mamindla <varunmamindla@gmail.com>
2018-06-25 18:13:36 -0500

    Initial Commit

---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Varun Mamindla on 6/25/18.
+//  Copyright © 2018 Varun Mamindla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Weather
+//
+//  Created by Varun Mamindla on 6/25/18.
+//  Copyright © 2018 Varun Mamindla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
